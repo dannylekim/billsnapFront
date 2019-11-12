2748dab5d0313283a91abb0fe619c6368aca073d
Kenny Nguyen <alphakennyn@gmail.com>
2019-11-12 01:10:10 -0500

    initialize tests for login

--- src/pages/Login/Login.test.jsx
+++ src/pages/Login/Login.test.jsx
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom';
 import LoginPage from './Login.jsx';
 
 describe('LoginPage', () => {
+  let wrapper;
+  let handleMockFunction;
+
+  beforeEach(() => {
+    handleMockFunction = jest.fn();
+    wrapper = shallow(<LoginPage />);
+  })
+
+  afterEach(() => {
+    handleMockFunction.mockRestore();
+  })
+
   describe('render', () => {
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<LoginPage />, div);
-      ReactDOM.unmountComponentAtNode(div);
+    it('should match snap shot', () => {
+      matches(<LoginPage />);
+    });
+  });
+
+  describe('functions', () => {
+    it('should call handler function when GenericButton is click', () => {
+      console.log(wrapper.instance());
+      wrapper.find('button.billsnap__generic-btn').simulate('click');
+      jest.spyOn(wrapper.instance().handleButtonClick).mockImplementation(handleMockFunction);
+      expect(handleMockFunction).toHaveBeenCalled();
     });
   });
 });
