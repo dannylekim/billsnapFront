6871e78e47cf5174a17aac3ad8d5e0372be49d2c
kenny nguyen <kenny.nguyen@dynamicly.com>
2019-11-19 16:56:51 -0500

    update some tests

--- src/views/Login/Login.test.jsx
+++ src/views/Login/Login.test.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginPage from './index.js';
+import LoginPage, { LoginContainer } from './Login.jsx';
 
 describe('LoginPage', () => {
   let wrapper;
+  let wrapperLoginContainer;
   let handleMockFunction;
 
   beforeEach(() => {
     handleMockFunction = jest.fn();
     wrapper = shallow(<LoginPage />);
+    wrapperLoginContainer = shallow(<LoginContainer />);
   })
 
   afterEach(() => {
@@ -16,17 +18,20 @@ describe('LoginPage', () => {
   })
 
   describe('render', () => {
-    it('should match snap shot', () => {
-      // matches(<LoginPage />);
-      expect(wrapper).toMatchSnapshot()
+    it('LoginPage should match snap shot', () => {
+      matches(<LoginPage />);
+      // expect(wrapper).toMatchSnapshot()
+    });
+    it('LoginContainer should match  snap shot ', () => {
+      matches(<LoginContainer />);
+      // expect(wrapperLoginContainer).toMatchSnapshot()
     });
   });
 
   describe('functions', () => {
     it('should call handler function when GenericButton is click', () => {
-      console.log(wrapper.html());
-      wrapper.find('.billsnap__generic-btn').simulate('click');
-      jest.spyOn(wrapper.instance().handleButtonClick).mockImplementation(handleMockFunction);
+      wrapperLoginContainer.find('GenericButton').simulate('click');
+      jest.spyOn(wrapperLoginContainer.instance().handleButtonClick).mockImplementation(handleMockFunction);
       expect(handleMockFunction).toHaveBeenCalled();
     });
   });
