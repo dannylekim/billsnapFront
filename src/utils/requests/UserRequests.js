3fdb232d9dd629daca921298a2c66b4376963fc9
thien <d.thien.ng97@gmail.com>
2019-12-02 21:26:16 -0500

    feat(#2w79hx_#347fp7): add post request

--- src/utils/requests/UserRequests.js
+++ src/utils/requests/UserRequests.js
@@ -1,3 +1,5 @@
+const { URL } = require("../../../config")
+
 /**
  * @function logUser
  * @description Function used to wrap the login request while simply requiring credentials as params
@@ -5,19 +7,57 @@
  * @param {String} credentials.password // no need to encrypt yet
  */
 export const login = async (credentials) => {
-    // should do something with credentials...
-    // hint: use GET
+    try { 
+        let x = await fetch(URL + "/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials),
+        })
+        checkStatus(x);
+        parseJSON(x);
+    }
+    catch(e){
+        return e;
+    }
 };
 
 /**
- * @function logUser
- * @description Function used to wrap the register request while simply requiring credentials as params
+ * @function registerUser
+ * @description Function used to wrap the register request while simply requiring data for account registration
  * @param {String} entries.email
  * @param {String} entries.password // no need to encrypt yet
  * @param {String} entries.confirmPassword
  * @param {String} entries.etc... name, age, ...rest
  */
 export const register = async (entries) => {
-    // should do something with entries...
-    // hint: POST
+    try { 
+        let x = await fetch(URL + "/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(entries),
+        })
+        checkStatus(x);
+        parseJSON(x);
+    }
+    catch(e){
+        return e;
+    }
 };
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+}
+
+function parseJSON(response) {
+    return response.json();
+}
