5218d57890a85d4d04addc8ab417b46fc40f05df
Brian Kim-Lim <brianjkl240@gmail.com>
2020-03-02 14:42:17 -0500

    feat(#2w79jf_#4p6r5f): add Logo Component (#18)
    * feat(#2w79jf_#4p6r5f): add empty Dashboard page
    
    * feat(#2w79jf_#4p6r5f): create Logo component and add to App header
    
    * feat(#2w79jf_#4p6r5f): change Logo component to use useEffect

---
+++ src/components/Logo/__tests__/Logo.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Logo} from '../Logo.jsx';
+
+describe('Logo', () => {
+    describe('render', () => {
+        describe('snapshots 📸', () => {
+            it('Logo default should match snap shot', () => {
+                matches(<Logo/>);
+            });
+
+            it('Logo size large should match snap shot', () => {
+                matches(<Logo size='large' />);
+            });
+
+            it('Logo size medium should match snap shot', () => {
+                matches(<Logo size='medium' />);
+            });
+
+            it('Logo size undefined option should match snap shot', () => {
+                matches(<Logo size='gibberish' />);
+            });
+        });
+    });
+});
--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-
 /**
  * Load you page components here. We will render them via routes
  */
 // import LoginPage from '../Login/Login';
 import RegisterPage from '../Register/Register';
+import { Logo } from '../../components/Logo/Logo.jsx';
 
 
 import './styles.scss';
 
 export default (props) => {
-  const appName = process.env.REACT_APP_NAME;
+    const appName = process.env.REACT_APP_NAME;
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {appName}
-      </header>
-      <RegisterPage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a className="header-name" href="/">{appName}</a>
+                <div className="App-logo">
+                    <Logo />
+                </div>
+            </header>
+            <RegisterPage/>
+        </div>
+    );
 }
