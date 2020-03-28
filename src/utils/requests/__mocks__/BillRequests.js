334f3748d51741c604239a3119e4adc0c2434cf1
Andy Nguyen <brobromario@hotmail.com>
2020-03-28 14:49:33 -0400

    mock request

---
+++ src/utils/requests/__mocks__/BillRequests.js
@@ -0,0 +1,38 @@
+export const mockGetBill = (token) => {
+    try{
+        if(token === "good")
+            return Promise.resolve(
+                [
+                    {
+                        id: 1,
+                        name: "string",
+                        status: "OPEN",
+                        category: "string",
+                        balance: 10.00
+                    },
+                    {
+                        id: 2,
+                        name: "string",
+                        status: "OPEN",
+                        category: "string",
+                        balance: 14.000
+                    }
+                ]
+            );
+        else if (token === "bad")
+            return Promise.resolve(
+                []
+            );
+        else 
+            return Promise.resolve(
+                {
+                    status: "UNAUTHORIZED",
+                    timestamp: "time",
+                    message: "Access is unauthorized!",
+                    errors: []
+                }
+            );
+    }catch(error){
+        throw(error)
+    };
+};
--- src/utils/requests/__tests__/BillRequests.test.js
+++ src/utils/requests/__tests__/BillRequests.test.js
@@ -1,26 +1,22 @@
-import { getBill } from "../BillRequests";
+import {mockGetBill} from "../__mocks__/BillRequests";
 
 describe("BillRequests", () => {
-    /** @description Note that these tokens may change. */ 
-    const userWithBills = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwYXNzd29yZEBQYXNzd29yZDEyMy5jb20iLCJleHAiOjExNTgzMzY4NTYyLCJyb2xlcyI6WyJST0xFX1VTRVIiXX0.v1ghtMgVNzrBBWBl2nrVOzCrNy6v3F5avY83CCfHbaIZ3SzvXkPi9MBwG6ZNmWDxEeSmozp3oCTCzp5kljg2rg";
-    const userWithNoBills = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3aWxsLnNtaXRoQGhvdG1haWwuY29tIiwiZXhwIjoxMTU4NDQ3NTEzMCwicm9sZXMiOlsiUk9MRV9VU0VSIl19.-WYZdPKTwJxs3R22Y_iBK0Mfao_xBMSMfglC0FTz0onK47Mw9mm_Y2xH1Z67G7oq7Mf47xTmXcDWqiRtM4CjFg";
-
+   
     describe("getBill", () => {
 
         it("Should contain no bills", async () => {
-            const res = await getBill(userWithNoBills);
+            const res = await mockGetBill("bad");
             expect(res).toEqual([]);
         });
 
         it("Should contain bills", async () => {
-            const res = await getBill(userWithBills);
+            const res = await mockGetBill("good");
             expect(res.length).toBeGreaterThan(0);
         });
 
         it("Should throw an unauthorized error", async () => {
-            const res = await getBill("invalidToken");
-            expect(res.status).toEqual("UNAUTHORIZED");
-            expect(res.message).toEqual("Access is unauthorized!");
+            const res = await mockGetBill("invalidToken") 
+            expect(typeof res).toEqual("object");
         });
     });
 });
