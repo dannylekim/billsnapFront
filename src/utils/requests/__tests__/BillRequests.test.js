7368106fdb74f758f2f6854ddcab78f14f72ae22
Andy Nguyen <brobromario@hotmail.com>
2020-03-28 15:20:46 -0400

    attempt to fix (Can't test coverage locally)

--- src/utils/requests/__tests__/BillRequests.test.js
+++ src/utils/requests/__tests__/BillRequests.test.js
@@ -1,21 +1,23 @@
 import {mockGetBill} from "../__mocks__/BillRequests";
+// import {getBill} from "../BillRequests";
 
 describe("BillRequests", () => {
-   
+    const getBill = jest.fn(token => mockGetBill(token));
+
     describe("getBill", () => {
 
         it("Should contain no bills", async () => {
-            const res = await mockGetBill("bad");
+            const res = await getBill("bad");
             expect(res).toEqual([]);
         });
 
         it("Should contain bills", async () => {
-            const res = await mockGetBill("good");
+            const res = await getBill("good");
             expect(res.length).toBeGreaterThan(0);
         });
 
         it("Should throw an unauthorized error", async () => {
-            const res = await mockGetBill("invalidToken") 
+            const res = await getBill("invalidToken") 
             expect(typeof res).toEqual("object");
         });
     });
