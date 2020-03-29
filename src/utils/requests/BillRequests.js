28de5c0eb780058ad3eeda3bf541343e8998eace
Andy Nguyen <nguyen.andy123@gmail.com>
2020-03-29 18:27:04 -0400

    feat(#2w79jf_#5e7pxh): add getBill request (#24)
    * feat(#5e7pxh): add BillRequest
    
    * fix(#5e7pxh): removed redundant test
    
    * revert package json: removed unused packages
    
    * feat(#5e7pxh): added some tests for bill request
    
    * fix: (#5e7pxh): spelling mistake
    
    * fix(#5e7pxh): added try catch on fetch
    
    * mock request
    
    * attempt to fix (Can't test coverage locally)
    
    * remove __test__ folder tol avoid coverage
    
    * remove __test__
    
    * attempt to resolve codecov
    
    * spelling error fix
    
    * rename fake tokens
    
    * attempt at modifying test
    
    * feat(#2w79jf_#5e7pxh): add mock tests to the getBill request
    
    Co-authored-by: Danny Le Kim <dannylekim@gmail.com>

---
+++ src/utils/requests/BillRequests.js
@@ -0,0 +1,18 @@
+const {URL} = require("../../config");
+/**
+ * @function getBill
+ * @description Function that calls the /bills route to return the bills associated to the user. With bearer token.
+ */
+export const getBill = async () => {
+    try{
+        const token = localStorage.getItem("token");
+        return await fetch(`${URL}/bills`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            }
+        });
+    }catch(error){
+        throw(error)
+    }
+};
