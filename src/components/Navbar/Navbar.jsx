f25aa2f8b6b3bd4af8e1327533699e31bb3dabfd
Kevinjhlin <31754632+Kevinjhlin@users.noreply.github.com>
2020-06-14 11:03:05 -0400

    Feat/#2w79jf #4p6r6q (#29)
    * feat(#2w79jf_#4q6r6q): Create files for the sidebar component
    
    * Migrate navbar to its own layer component
    
    * Add toggleable sidebar
    
    * feat(#2w79jf_#4q6r6q): Add css to sidebar and
    make the sidebar and navbar into its own layer
    
    * feat(#2w79jf_#4q6r6q): Add test files
    
    * feat(#2w79jf_#4q6r6q): Readd missing file
    
    * feat(#2w79jf_#4q6r6q): Re-add missing file
    
    * add invalid link plugin
    
    * Revert "add invalid link plugin"
    
    This reverts commit 63a4e42353d845f06b2c6766c3e1973d65ef3004.
    
    * Specify node version 12 on travis
    
    * add invalid link plugin
    
    Co-authored-by: Kenny Nguyen <alphakennyn@gmail.com>
    Co-authored-by: Kenny Nguyen <kenny.nguyen@makegoodfood.ca>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import './styles.scss';
-import {Nav, NavItem, NavLink} from "shards-react";
-import {FaBars, FaHome, FaMoneyBillWave} from 'react-icons/fa';
-export default () => {
-    return (
-        <Nav tabs>
-            <NavItem>
-                <NavLink href="#">
-                    <FaBars/>
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#">
-                    <FaHome/>
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink>
-                    <FaMoneyBillWave/>
-                </NavLink>
-            </NavItem>
-        </Nav>
-    )
-}
+import { Nav, Navbar,  NavItem, NavLink } from 'shards-react';
+import { FaBars, FaHome, FaMoneyBillWave } from 'react-icons/fa';
+
+export default ({ toggleSideBar }) => {
+
+    const handleBurger = (e) => {
+        e.preventDefault();
+        toggleSideBar();
+    }
+
+	return (
+		<Navbar type="light" expand="sm">
+			<Nav navbar>
+				<NavItem>
+					<NavLink href="#" onClick={handleBurger}>
+						<FaBars />
+					</NavLink>
+				</NavItem>
+				<NavItem>
+                    <NavLink
+                        href="#"
+                    >
+						<FaHome />
+					</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink>
+						<FaMoneyBillWave />
+					</NavLink>
+				</NavItem>
+			</Nav>
+		</Navbar>
+	);
+};
--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -3,20 +3,14 @@ import React from 'react';
  * Load you page components here. We will render them via routes
  */
 import RegisterPage from '../Register/Register';
-import Navbar from '../../components/Navbar/Navbar.jsx';
-import { Logo } from '../../components/Logo/Logo.jsx';
+import NavbarLayer from '../NavbarLayer';
 
 import './styles.scss';
 
 export default (props) => {
-  
   return (
     <div className="App">
-        <div className="App-logo">
-            <Logo />
-        </div>
-        <Navbar className="App-navbar"/>
-
+      <NavbarLayer />
       <RegisterPage />
     </div>
   );
