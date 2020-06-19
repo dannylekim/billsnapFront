710be9f7f72b89bd6631e75a310e862d6d0f0d45
Kenny Nguyen <alphakennyn@gmail.com>
2020-06-18 23:24:15 -0400

    Fix/eslint config (#34)
    * fix(eslint_config): set up rules in eslint configuration
    
    * fix(eslint_config): update yarn lock and clear useless configs
    
    Co-authored-by: Danny Le Kim <dannylekim@gmail.com>
    Co-authored-by: Kenny Nguyen <kenny.nguyen@makegoodfood.ca>

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,30 +7,26 @@ import './styles.scss';
 const SideBar = ({collapseState}) => {
     return (
         <div className={`billSnap-SideBar billSnap-SideBar-${collapseState ? 'open' : 'close'}`}>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a href="#">
                 <FaUserCircle/>
             </a>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a href="#">
                 Profile
             </a>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a href="#">
                 Settings
             </a>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a href="#">
                 Log Out
             </a>
         </div>
     )
-}
+};
 
 SideBar.propTypes = {
     // vertical or horizontal
     type: PropTypes.string,
-}
+};
 
 SideBar.defaultProps = {
     type: "horizontal",
