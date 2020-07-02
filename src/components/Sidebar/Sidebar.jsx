17076ce76a04550da8133349ca6e1b5b912b0c65
Kevinjhlin <jinghao.lin@mail.utoronto.ca>
2020-07-02 18:37:38 -0400

    feat(#8feat/#8rdhyr_#90jnbn): Restructure Sidebar to include all relevant selections

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,45 @@
 import React from 'react';
 import {FaUserCircle} from 'react-icons/fa';
-import {Nav, NavItem, NavLink} from 'shards-react';
+import {Nav, Navbar, NavbarBrand, NavItem, NavLink} from 'shards-react';
 
 import './styles.scss';
 
 export default () => {
     return (
-        <Nav className={"billSnap-SideBar"} vertical={true}>
-            <NavItem>
-                <NavLink href="#">
-                    <FaUserCircle/>
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#">
-                    Profile
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#">
-                    Settings
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#">
-                    Log Out
-                </NavLink>
-            </NavItem>
-        </Nav>
+
+        <Navbar>
+            <Nav className={"billSnap-SideBar"} vertical={true}>
+
+                <NavbarBrand>
+                    <NavLink href="#">
+                        Billsnap Logo
+                    </NavLink>
+                </NavbarBrand>
+
+                <NavItem>
+                    <NavLink href="#">
+                        <FaUserCircle/>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="#">
+                        Profile
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="#">
+                        Settings
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="#">
+                        Log Out
+                    </NavLink>
+                </NavItem>
+            </Nav>
+
+        </Navbar>
+
+
     )
 };
