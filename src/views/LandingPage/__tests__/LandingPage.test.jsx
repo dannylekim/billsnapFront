026e57308226be0a2dd8169460e5d4a8b5533829
Kenny Nguyen <kenny.nguyen@makegoodfood.ca>
2020-07-05 21:55:54 -0400

    Update old test to pass with redux

--- src/views/LandingPage/__tests__/LandingPage.test.jsx
+++ src/views/LandingPage/__tests__/LandingPage.test.jsx
@@ -3,11 +3,20 @@ import LandingPage from "../LandingPage.jsx";
 import {createRegisterFormElements} from "../../../constants/FormElements";
 
 describe("LandingPage", () => {
+  let mockFn;
+  beforeEach(() => {
+    mockFn = jest.fn();
+  });
+
   describe("render", () => {
     describe("snapshots 📸", () => {
-      it("LandingPage should match snap shot", () => {
+      it("LandingPage should match snap shot register", () => {
         createRegisterFormElements();
-        matches(<LandingPage />);
+        matches(<LandingPage formType={'register'} toggleFormType={mockFn} />);
+      });
+      it("LandingPage should match snap shot login", () => {
+        createRegisterFormElements();
+        matches(<LandingPage formType={'login'} toggleFormType={mockFn} />);
       });
     });
   });
