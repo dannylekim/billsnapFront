ed144f7f39b7ee3077df015a12aed7e7b06bcdc1
Kenny Nguyen <kenny.nguyen@makegoodfood.ca>
2020-07-06 00:06:17 -0400

    Add user reducers for future tasks

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import bills from "./billReducer";
+import users from "./userReducers";
 import application from "./applicationReducers";
 
 export default combineReducers({
-    bills,
     application,
+    bills,
+    users,
 });
