afa9a4e950bfb3793abba2cd77c2c9706001c55f
Kevinjhlin <jinghao.lin@mail.utoronto.ca>
2020-07-11 23:37:59 -0400

    feat(#8feat/#8rdhyr_#90jnbn): Add hover functionality on sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,49 +1,81 @@
 import React from 'react';
-import {Face, Help, People, Receipt, Settings} from '@material-ui/icons';
+import {MdFace, MdHelp, MdPeople, MdReceipt, MdSettings} from 'react-icons/md';
 import {Nav, Navbar, NavbarBrand, NavItem, NavLink} from 'shards-react';
 
 import './styles.scss';
 //import {FaUserCircle} from "react-icons/all";
 
-export default () => {
-    return (
+const DEFAULT_ACTIVE_STATE = {
+    bills: false,
+    profile: false,
+    contacts: false,
+    settings: false,
+    help: false
+}
 
-        <Navbar>
-            <Nav tabs className={"billSnap-SideBar"} vertical={true}>
+class Sidebar extends React.Component{
 
-                <NavbarBrand className={"sideLogo"}>
+    constructor(props) {
+        super(props)
+        this.state = {
+            activeState: {
+                ...DEFAULT_ACTIVE_STATE
+            }
+        }
+        this.handleClick=this.handleClick.bind(this)
+    }
+
+    handleClick(link) {
+        this.setState({
+            activeState: {
+                ...DEFAULT_ACTIVE_STATE,
+                [link]:true
+            }
+        })
+    }
+
+    render() {
+        return (
+
+            <Navbar>
+                <Nav tabs className={"billSnap-SideBar"} vertical={true}>
+
+                    <NavbarBrand className={"sideLogo"}>
                         Billsnap Logo
-                </NavbarBrand>
+                    </NavbarBrand>
 
-                <NavItem>
-                    <NavLink href="/dashboard">
-                        <Receipt/> Bills
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/dashboard">
-                        <Face/> Profile
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">
-                        <People/> Contacts
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">
-                        <Settings/> Settings
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">
-                        <Help/> Help
-                    </NavLink>
-                </NavItem>
-            </Nav>
+                    <NavItem>
+                        <NavLink onClick={()=>this.handleClick("bills")} active={this.state.activeState.bills}>
+                            <MdReceipt/> Bills
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink onClick={()=>this.handleClick("profile")} active={this.state.activeState.profile}>
+                            <MdFace/> Profile
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <MdPeople/> Contacts
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <MdSettings/> Settings
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <MdHelp/> Help
+                        </NavLink>
+                    </NavItem>
+                </Nav>
 
-        </Navbar>
+            </Navbar>
 
 
-    )
-};
+        )
+    }
+}
+
+export default Sidebar
