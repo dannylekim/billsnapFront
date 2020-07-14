981c7ef63b90696c0edb2f277bff3e7e3d71b438
Kevin Lin <31754632+Kevinjhlin@users.noreply.github.com>
2020-07-14 11:26:32 -0400

    Feat/#8rdhyr #90jnbn (#48)
    * feat(#8feat/#8rdhyr_#90jnbn): Restructure Sidebar to include all relevant selections
    
    * feat(#8feat/#8rdhyr_#90jnbn): Add background to match figma
    
    * feat(#8feat/#8rdhyr_#90jnbn): Add spacing to logo
    
    * feat(#8feat/#8rdhyr_#90jnbn): Add Sidebar to dashboard
    
    * feat(#8feat/#8rdhyr_#90jnbn): Update variables
    
    * feat(#8feat/#8rdhyr_#90jnbn): Add hover functionality on sidebar
    
    * feat(#8feat/#8rdhyr_#90jnbn): Add sidebar feature and update sidebar test
    
    * feat(#8feat/#8rdhyr_#90jnbn): Fix hover shadow on sidebar to match rounded end

--- src/components/Sidebar/Sidebar.test.jsx
+++ src/components/Sidebar/Sidebar.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Sidebar from './Sidebar.jsx';
+import Sidebar, {DEFAULT_ACTIVE_STATE} from './Sidebar.jsx';
+import {NavLink} from "shards-react";
 
 describe('Sidebar', () => {
     let wrapper;
@@ -15,6 +16,60 @@ describe('Sidebar', () => {
             it('Sidebar should match snap shot', () => {
                 matches(<Sidebar/>);
             });
+            it('Sidebar should match snap shot', () => {
+                const EXPECTED_STATE = {
+                    ...DEFAULT_ACTIVE_STATE,
+                    contacts: true
+                };
+                wrapper.setState({
+                    activeState: {...EXPECTED_STATE}
+                });
+                matches(wrapper);
+            });
+        })
+        describe('interactions', () => {
+            it('Navlink should call handleClick on click', () => {
+                const mockFunction = jest.fn();
+                wrapper.instance().handleClick = mockFunction;
+                wrapper.find(NavLink).filter('#billSnap-SideBar__bills').simulate('click');
+                wrapper.find(NavLink).filter('#billSnap-SideBar__profile').simulate('click');
+                wrapper.find(NavLink).filter('#billSnap-SideBar__contacts').simulate('click');
+                wrapper.find(NavLink).filter('#billSnap-SideBar__settings').simulate('click');
+                wrapper.find(NavLink).filter('#billSnap-SideBar__help').simulate('click');
+                expect(mockFunction).toHaveBeenCalledTimes(5);
+            });
         })
     })
+
+    describe('function', () => {
+        describe('handleClick', () => {
+            it('should not change state if parameter does not exist in this state', () => {
+                const EXPECTED_STATE = {
+                    ...DEFAULT_ACTIVE_STATE,
+                    contacts: true
+                };
+                wrapper.setState({
+                    activeState: {...EXPECTED_STATE}
+                })
+                wrapper.instance().handleClick('starlord');
+                expect(wrapper.state('activeState')).toEqual(EXPECTED_STATE)
+            });
+            it('should change state if parameter does exist in this state', () => {
+                const DEFAULT_STATE = {
+                    ...DEFAULT_ACTIVE_STATE,
+                    contacts: true
+                }
+                const EXPECTED_STATE = {
+                    ...DEFAULT_ACTIVE_STATE,
+                    bills: true
+                }
+                wrapper.setState({
+                    activeState: {...DEFAULT_STATE}
+                })
+                wrapper.instance().handleClick('bills');
+                expect(wrapper.state('activeState')).toEqual(EXPECTED_STATE)
+            });
+        })
+    })
+
 })
