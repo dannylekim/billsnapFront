d08479018bc80ee68951f195614b6d719e77b1d0
Kevinjhlin <jinghao.lin@mail.utoronto.ca>
2020-07-18 12:05:17 -0400

    feat(#8rdhyr_#5cbhx2): Add Logout to Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {MdFace, MdHelp, MdPeople, MdReceipt, MdSettings} from 'react-icons/md';
+import {FiLogOut} from 'react-icons/fi';
 import {Nav, Navbar, NavbarBrand, NavItem, NavLink} from 'shards-react';
 import './styles.scss';
 
@@ -8,7 +9,8 @@ export const DEFAULT_ACTIVE_STATE = {
     profile: false,
     contacts: false,
     settings: false,
-    help: false
+    help: false,
+    logout: false
 }
 
 class Sidebar extends React.Component {
@@ -22,6 +24,7 @@ class Sidebar extends React.Component {
             }
         }
         this.handleClick = this.handleClick.bind(this)
+        this.handleLogoutClick = this.handleLogoutClick.bind(this)
     }
 
     handleClick(link) {
@@ -35,7 +38,12 @@ class Sidebar extends React.Component {
         }
     }
 
+    handleLogoutClick() {
+        localStorage.clear();
+    }
+
     render() {
+
         return (
             <Navbar>
                 <Nav tabs className={"billSnap-SideBar"} vertical={true}>
@@ -45,28 +53,39 @@ class Sidebar extends React.Component {
 
                     <div className="sidebar__navitems">
                         <NavItem>
-                            <NavLink id='billSnap-SideBar__bills' onClick={() => this.handleClick("bills")} active={this.state.activeState.bills}>
+                            <NavLink id='billSnap-SideBar__bills' onClick={() => this.handleClick("bills")}
+                                     active={this.state.activeState.bills}>
                                 <MdReceipt/> Bills
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink id='billSnap-SideBar__profile' onClick={() => this.handleClick("profile")} active={this.state.activeState.profile}>
+                            <NavLink id='billSnap-SideBar__profile' onClick={() => this.handleClick("profile")}
+                                     active={this.state.activeState.profile}>
                                 <MdFace/> Profile
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink id='billSnap-SideBar__contacts' onClick={() => this.handleClick("contacts")} active={this.state.activeState.contacts}>
+                            <NavLink id='billSnap-SideBar__contacts' onClick={() => this.handleClick("contacts")}
+                                     active={this.state.activeState.contacts}>
                                 <MdPeople/> Contacts
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink id='billSnap-SideBar__settings' onClick={() => this.handleClick("settings")} active={this.state.activeState.settings}>
+                            <NavLink id='billSnap-SideBar__settings' onClick={() => this.handleClick("settings")}
+                                     active={this.state.activeState.settings}>
                                 <MdSettings/> Settings
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink id='billSnap-SideBar__help' onClick={() => this.handleClick("help")} active={this.state.activeState.help}>
+                            <NavLink id='billSnap-SideBar__help' onClick={() => this.handleClick("help")}
+                                     active={this.state.activeState.help}>
                                 <MdHelp/> Help
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink id='billSnap-SideBar__logout' onClick={() => this.handleLogoutClick()}
+                                     active={this.state.activeState.logout}  href={"/"}>
+                                <FiLogOut/> Log out
                             </NavLink>
                         </NavItem>
                     </div>
