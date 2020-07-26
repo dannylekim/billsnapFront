f75431c131de7c6ac8c7fe4b157616f5166ced4e
Andy Nguyen <brobromario@hotmail.com>
2020-07-26 11:24:11 -0400

    redirect if already logged in and snapshot

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -3,23 +3,21 @@ import Sidebar from "../../components/Sidebar";
 import BillDisplay from "../BillDisplay";
 import "./styles.scss";
 
-export default props => {
-
-    return (
-        <div>
-            {localStorage.getItem("billSnap_token") ? 
-               ( 
-               <div className="dashboard__flexbox">
-                    <div className="side__bar">
-                        <Sidebar/> 
-                    </div>
-                    <div className="bill__display">
-                        <BillDisplay/>
-                    </div>
-                </div>
-               )
-                  :
-                  props.history.push("/")}
-        </div> 
-    )
-}
+export default (props) => {
+  return (
+    <div>
+      {localStorage.getItem("billSnap_token") ? (
+        <div className="dashboard__flexbox">
+          <div className="side__bar">
+            <Sidebar />
+          </div>
+          <div className="bill__display">
+            <BillDisplay />
+          </div>
+        </div>
+      ) : (
+        props.history.push("/")
+      )}
+    </div>
+  );
+};
