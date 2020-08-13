d7902cbdcf80577b080a8bf14d8f7162f64c75a9
Kenny Nguyen <kenny.nguyen@makegoodfood.ca>
2020-08-13 10:00:32 -0400

    Clean up SmallCard

--- src/components/BillCard/SmallBillCard/SmallBillCard.jsx
+++ src/components/BillCard/SmallBillCard/SmallBillCard.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import PropType from "prop-types";
+import {FaBus, FaCar, FaQuestion, FaShoppingBag, FaShoppingCart, FaUtensils } from "react-icons/fa";
+
+import { filterDateTime } from '../../../helpers/DateTime';
+
 import "./styles.scss";
 
-const SmallBillCard = ({ activeBill, bill, filterDateTime, billIcons }) => (
+const getBillIcons = (category) => {
+  const color = "rgba(0, 0, 0, 0.96)";
+  switch (category) {
+    case "food":
+      return <FaUtensils color={color} size={24} />;
+    case "transport":
+      return <FaCar color={color} size={24} />;
+    case "public-transport":
+      return <FaBus color={color} size={24} />;
+    case "grocery":
+      return <FaShoppingCart color={color} size={24} />;
+    case "shopping":
+      return <FaShoppingBag color={color} size={24} />;
+    default:
+      return <FaQuestion color={color} size={24} />;
+  }
+};
+
+const SmallBillCard = ({ activeBill, bill }) => (
   <div
     className="bill__items card-body"
     id={
@@ -10,7 +33,7 @@ const SmallBillCard = ({ activeBill, bill, filterDateTime, billIcons }) => (
         : "non__active__bill"
     }
   >
-    {bill.created && (
+    {!!bill.created && (
       <div className="bill__items card-title text-muted">
         <span className="float-right" id="bill__created">
           {" "}
@@ -19,7 +42,7 @@ const SmallBillCard = ({ activeBill, bill, filterDateTime, billIcons }) => (
       </div>
     )}
     <div className="bill__items card-text">
-      <div id="bill__icon">{billIcons(bill.category)}</div>
+      <div id="bill__icon">{getBillIcons(bill.category)}</div>
       <div className="bill__name__price">
         <span id="bill__name">{bill.name} </span>
         <span id="bill__balance">
@@ -36,4 +59,10 @@ const SmallBillCard = ({ activeBill, bill, filterDateTime, billIcons }) => (
     )}
   </div>
 );
+
+SmallBillCard.propTypes = {
+  bill: PropType.arrayOf(PropType.shape({})), // array of bill object
+  activeBill: PropType.shape({}) // bill selected
+}
+
 export default SmallBillCard;
