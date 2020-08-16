23da2fcb67ce74beafaa9f8f116f2844d77404b9
Danny Lekim <dannylekim@gmail.com>
2020-08-16 11:56:33 -0400

    Feat/#8ug661 #b0n1zj (#58)
    * feat(#8ug661_#b0n1zj): start PendingBillCard.jsx, missing flexbox css to properly match the design
    
    * feat(#8ug661_#b0n1zj): add css and proper structuring
    
    * feat(#8ug661_#b0n1zj): add snapshot test

---
+++ src/components/PendingBillCard/PendingBillCard.test.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {PendingBillCard} from "./PendingBillCard";
+
+describe("PendingBillCard", () => {
+
+  let wrapper;
+
+  describe("render", () => {
+    const bill = {
+      id: 0,
+      name: "Test Bill",
+      responsible: {
+        firstName: "Test User",
+      },
+      status: "IN_PROGRESS",
+      category: "string",
+      created: "2020-08-14T15:28:56.580Z",
+      balance: 100,
+      amountOwed: 50
+    };
+
+    describe("snapshots 📸", () => {
+      it("PendingBillCard should match snap shot", () => {
+        matches(
+          <PendingBillCard
+            bill={bill}
+            activeBillId={bill.id}
+            acceptBillInvitationHandler={jest.fn()}
+            declineBillInvitationHandler={jest.fn()}
+            filterDateHandler={jest.fn()}
+            getBillIconHandler={jest.fn()}
+          />
+        );
+      });
+    });
+
+    describe("component ", () => {
+      it("should not find an active bill", () => {
+        wrapper = shallow(
+            <PendingBillCard
+                bill={bill}
+                activeBillId={999}
+                acceptBillInvitationHandler={jest.fn()}
+                declineBillInvitationHandler={jest.fn()}
+                filterDateHandler={jest.fn()}
+                getBillIconHandler={jest.fn()}
+            />
+        );
+        expect(wrapper.find("#non__active__bill")).toHaveLength(1);
+      });
+
+      it("should find an active bill", () => {
+        wrapper = shallow(
+            <PendingBillCard
+                bill={bill}
+                activeBillId={bill.id}
+                acceptBillInvitationHandler={jest.fn()}
+                declineBillInvitationHandler={jest.fn()}
+                filterDateHandler={jest.fn()}
+                getBillIconHandler={jest.fn()}
+            />
+        );
+        expect(wrapper.find("#active__bill")).toHaveLength(1);
+      });
+    });
+  });
+});
