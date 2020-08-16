9a49bc2c494ee92d980e078c81113c863b7f1e1d
Kevinjhlin <jinghao.lin@mail.utoronto.ca>
2020-08-16 11:46:46 -0400

    feat(#a4k5en_#a6k6a5): Add SVG on top right corner

---
+++ src/views/CreateBillBackground/CreateBillBackground.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./styles.scss";
+
+export default () => {
+    return (
+        <div>
+            <svg
+                width="250"
+                height="50"
+                viewBox="0 0 250 50"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="billsnap-bottomcorner"
+            >
+                <path
+                    d="M0.856934 0.180298H249.824V42.9967C192.418 55.1127 137.48 51.2066 118.278 33.3318C99.0753 15.457 86.936 17.0159 55.5944 20.6532C24.2528 24.2905 0.856934 0.180298 0.856934 0.180298Z"
+                    fill="#2FCE9F"
+                />
+            </svg>
+            <svg
+                width="265"
+                height="50"
+                viewBox="0 0 265 50"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="billsnap-topcorner"
+            >
+                <path
+                    d="M0.944824 0H264.824V42.8164C203.98 54.9324 145.751 51.0263 125.398 33.1515C105.046 15.2767 92.1796 16.8356 58.9608 20.4729C25.742 24.1102 0.944824 0 0.944824 0Z"
+                    fill="#75ECC8"
+                />
+            </svg>
+
+        </div>
+    )
+}
---
+++ src/views/CreateBillBackground/__tests__/CreateBillBackground.test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateBillBackground from "../CreateBillBackground.jsx";
+
+describe("Background", () => {
+    describe("render", () => {
+        describe("snapshots 📸", () => {
+            it("CreateBillBackground should match snapshot", () => {
+                matches(<CreateBillBackground/>);
+            });
+        });
+    });
+});
