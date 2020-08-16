321e568b4a2b669f35771122f0b46c9cfe212ea9
Kenny Nguyen <kenny.nguyen@makegoodfood.ca>
2020-08-16 12:06:00 -0400

    Extract bill icons into helper functions

--- src/components/BillCard/SmallBillCard/SmallBillCard.jsx
+++ src/components/BillCard/SmallBillCard/SmallBillCard.jsx
@@ -1,28 +1,9 @@
 import React from "react";
 import PropType from "prop-types";
-import {FaBus, FaCar, FaQuestion, FaShoppingBag, FaShoppingCart, FaUtensils } from "react-icons/fa";
 
 import { filterDateTime } from '../../../helpers/DateTime';
-
+import { getBillIcons } from '../../../helpers/Components';
 import "./styles.scss";
-
-const getBillIcons = (category) => {
-  const color = "rgba(0, 0, 0, 0.96)";
-  switch (category) {
-    case "food":
-      return <FaUtensils color={color} size={24} />;
-    case "transport":
-      return <FaCar color={color} size={24} />;
-    case "public-transport":
-      return <FaBus color={color} size={24} />;
-    case "grocery":
-      return <FaShoppingCart color={color} size={24} />;
-    case "shopping":
-      return <FaShoppingBag color={color} size={24} />;
-    default:
-      return <FaQuestion color={color} size={24} />;
-  }
-};
 
 const SmallBillCard = ({ activeBillId, bill }) => (
   <div
