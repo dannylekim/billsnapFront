e59592696af643f89db6fe77c4cd862eabdffec1
Danny Lekim <dannylekim@gmail.com>
2020-08-20 21:06:01 -0400

    feat(#arnjrd_#b0n2e4): add PayBill presentational component (#63)
    * feat(#arnjrd_#b0n2e4): add PayBill presentational component
    
    * feat(#arnjrd_#b0n2e4): add snapshot with values included

---
+++ src/components/PayBill/PayBill.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {Button, FormInput} from "shards-react";
+import "./styles.scss";
+
+export default ({
+  payBillHandler,
+  onChangeHandler,
+  billName,
+  amountRemainingToPay,
+}) => (
+  <div>
+    <div className="split-info text__information">
+      <div>Bill Name</div>
+      <div>{billName}</div>
+    </div>
+    <div className="split-info text__information">
+      <div>Amount left to pay</div>
+      <FormInput
+        value={amountRemainingToPay}
+        onChange={onChangeHandler}
+        className="small-input"
+        step="0.01"
+        type="number"
+      />
+    </div>
+    <div className="split-info">
+      <div />
+      <Button
+        outline
+        pill
+        theme="success"
+        className="pay-button"
+        onClick={payBillHandler}
+      >
+        Pay Bill
+      </Button>
+    </div>
+  </div>
+);
