db25390a3b4d31e5bfab4447f0868178eae1547d
Danny Le Kim <dannylekim@gmail.com>
2020-08-26 23:24:59 -0400

    feat(#8ug661): change PendingBillCard component tests for snapshots

--- src/components/PendingBillCard/PendingBillCard.test.jsx
+++ src/components/PendingBillCard/PendingBillCard.test.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import {PendingBillCard} from "./PendingBillCard";
 
 describe("PendingBillCard", () => {
-
-  let wrapper;
-
   describe("render", () => {
     const bill = {
       id: 0,
@@ -16,7 +13,7 @@ describe("PendingBillCard", () => {
       category: "string",
       created: "2020-08-14T15:28:56.580Z",
       balance: 100,
-      amountOwed: 50
+      amountOwed: 50,
     };
 
     describe("snapshots 📸", () => {
@@ -32,35 +29,18 @@ describe("PendingBillCard", () => {
           />
         );
       });
-    });
 
-    describe("component ", () => {
-      it("should not find an active bill", () => {
-        wrapper = shallow(
-            <PendingBillCard
-                bill={bill}
-                activeBillId={999}
-                acceptBillInvitationHandler={jest.fn()}
-                declineBillInvitationHandler={jest.fn()}
-                filterDateHandler={jest.fn()}
-                getBillIconHandler={jest.fn()}
-            />
+      it("PendingBillCard should match snap shot", () => {
+        matches(
+          <PendingBillCard
+            bill={bill}
+            activeBillId={999}
+            acceptBillInvitationHandler={jest.fn()}
+            declineBillInvitationHandler={jest.fn()}
+            filterDateHandler={jest.fn()}
+            getBillIconHandler={jest.fn()}
+          />
         );
-        expect(wrapper.find(".non__active__bill")).toHaveLength(1);
-      });
-
-      it("should find an active bill", () => {
-        wrapper = shallow(
-            <PendingBillCard
-                bill={bill}
-                activeBillId={bill.id}
-                acceptBillInvitationHandler={jest.fn()}
-                declineBillInvitationHandler={jest.fn()}
-                filterDateHandler={jest.fn()}
-                getBillIconHandler={jest.fn()}
-            />
-        );
-        expect(wrapper.find(".active__bill")).toHaveLength(1);
       });
     });
   });
