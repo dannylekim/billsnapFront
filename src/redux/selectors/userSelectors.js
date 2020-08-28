a42ff587f190404da36fb66adef09aac98e2bfef
Kenny Nguyen <kenny.nguyen@makegoodfood.ca>
2020-08-28 15:26:38 -0400

    user logged should just depend on token

--- src/redux/selectors/userSelectors.js
+++ src/redux/selectors/userSelectors.js
@@ -3,5 +3,5 @@ import { createSelector } from 'reselect'
 const loggedUser = (state) => state.users; 
 
 export const isUserLogged = createSelector([loggedUser], (user) => {
-    return user && Object.keys(user.userInfo).length > 0 && localStorage.getItem("billSnap_token");
+    return localStorage.getItem("billSnap_token");
 });
