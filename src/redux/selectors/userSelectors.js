769a56fb4aab0318fd3d8d4e45ddfb1f448bd2e2
Kenny Nguyen <alphakennyn@gmail.com>
2020-08-28 13:07:31 -0400

    Fix selector for logged user (#73)
    * Fix selector for logged user
    
    * Fix odd margin-btm sidebar
    
    Co-authored-by: Kenny Nguyen <kenny.nguyen@makegoodfood.ca>

--- src/redux/selectors/userSelectors.js
+++ src/redux/selectors/userSelectors.js
@@ -2,4 +2,6 @@ import { createSelector } from 'reselect'
 
 const loggedUser = (state) => state.users; 
 
-export const isUserLogged = createSelector([loggedUser], (user) => user && Object.keys(user.userInfo).length > 0) && localStorage.getItem("billSnap_token");
+export const isUserLogged = createSelector([loggedUser], (user) => {
+    return user && Object.keys(user.userInfo).length > 0 && localStorage.getItem("billSnap_token");
+});
